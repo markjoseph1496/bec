8a569017e12428c8e0183a0b897405ef70f5abaf
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2016-07-03 21:22:59 +0800

    Initial

---
+++ css.php
@@ -0,0 +1,2 @@
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+include('connection.php');
+include('css.php');
+?>
+<html lang="en">
+    <title></title>
+    <head>
+
+    </head>
+<body>
+<div>
+    <button class="main-button">sadas
+    </button>
+</div>
+
+<button class='btn btn-danger' data-toggle='modal'
+        data-target='#Delete'><i
+        class='fa fa-trash fa-1x'>testing</i>
+</button>
+</body>
+</html>
