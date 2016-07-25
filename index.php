d3b17317b6a3193d56dde1e407ad2736a8ce6a82
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2016-07-25 14:06:52 +0800

    INITIAL COMMIT

--- index.php
+++ index.php
@@ -3,18 +3,14 @@ include('connection.php');
 include('css.php');
 ?>
 <html lang="en">
-    <title></title>
-    <head>
+<title></title>
+<head>
 
-    </head>
+</head>
 <body>
-<div>
-    <button class="main-button">sadas
-    </button>
-</div>
 
-<button class='btn btn-danger' data-toggle='modal'
-        data-target='#Delete'><i
+
+<button class='btn btn-danger' data-toggle='modal' data-target='#Delete'><i
         class='fa fa-trash fa-1x'>testing</i>
 </button>
 </body>
