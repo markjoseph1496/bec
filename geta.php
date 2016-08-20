c4353209814f2dc61b291e5e7e3c4dfb2e2c943d
Mark Joseph Cinco <markjoseph1496@yahoo.com>
2016-08-20 18:25:13 +0800

    q

---
+++ geta.php
@@ -0,0 +1,13 @@
+<?php
+include('connection.php');
+if(isset($_POST['id'])){
+    $id = $_POST['id'];
+    $unitprice =
+        GSecureSQL::query(
+            "SELECT SRP FROM unitstbl WHERE Model = '$id'",
+            TRUE
+        );
+
+    $Price = $unitprice[0][0];
+    echo $Price;
+}
